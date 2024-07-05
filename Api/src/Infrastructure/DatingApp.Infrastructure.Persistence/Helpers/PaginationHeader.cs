2a59f779e6fa555cbd4988a11340dfd85e45fcd1
Mahmoud Sallam <m.sallam@qoad.com>
2024-07-05 22:17:46 +0300

    Start Implementing Pagination & Sorting

---
+++ Api/src/Infrastructure/DatingApp.Infrastructure.Persistence/Helpers/PaginationHeader.cs
@@ -0,0 +1,18 @@
+﻿namespace DatingApp.Infrastructure.Persistence.Helpers
+{
+    public class PaginationHeader
+    {
+        public PaginationHeader(int currentPage, int itemPerPage, int totalItems, int tOTALPageS)
+        {
+            CurrentPage = currentPage;
+            ItemPerPage = itemPerPage;
+            TotalItems = totalItems;
+            this.tOTALPageS = tOTALPageS;
+        }
+
+        public int CurrentPage { get; set; }
+        public int ItemPerPage { get; set; }
+        public int TotalItems { get; set; }
+        public int tOTALPageS { get; set; }
+    }
+}
---
+++ Api/src/Infrastructure/DatingApp.Infrastructure.Persistence/Helpers/UserParams.cs
@@ -0,0 +1,6 @@
+﻿namespace DatingApp.Infrastructure.Persistence.Helpers
+{
+    public class UserParams
+    {
+    }
+}
